5257b94dc9c45d73b010514aaacdd7038f825226
Francesco Zoccheddu <zoccheddu.fra@gmail.com>
2018-04-01 16:18:50 +0200

    Rename project

--- C4D2C.c
+++ C4D2C.c
@@ -1,4 +1,4 @@
-#include "C4D2GL.h"
+#include "C4D2C.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,76 +6,76 @@
 #pragma warning(disable : 4996)
 #endif
 
-#define _C4D2GL_HEAD_DECL "C4D2GL v1.0"
-#define _C4D2GL_HEAD_DECL_LEN (sizeof(_C4D2GL_HEAD_DECL) / sizeof(char) - 1)
-#define _C4D2GL_FIELD_SEP '\n'
-#define _C4D2GL_VALUE_SEP ','
+#define _C4D2C_HEAD_DECL "C4D2C v1.0"
+#define _C4D2C_HEAD_DECL_LEN (sizeof(_C4D2C_HEAD_DECL) / sizeof(char) - 1)
+#define _C4D2C_FIELD_SEP '\n'
+#define _C4D2C_VALUE_SEP ','
 
-int _c4d2gl_sep (FILE * file, char sep);
+int _c4d2c_sep (FILE * file, char sep);
 
-int _c4d2gl_head_decl (FILE * file);
+int _c4d2c_head_decl (FILE * file);
 
-int _c4d2gl_ushort (FILE * file, unsigned short * out);
+int _c4d2c_ushort (FILE * file, unsigned short * out);
 
-int _c4d2gl_float (FILE * file, float * out);
+int _c4d2c_float (FILE * file, float * out);
 
-int _c4d2gl_sep (FILE * file, char sep)
+int _c4d2c_sep (FILE * file, char sep)
 {
 	return getc (file) != sep;
 }
 
-int _c4d2gl_head_decl (FILE * file)
+int _c4d2c_head_decl (FILE * file)
 {
 	int pos;
 	pos = 0;
-	while (pos < _C4D2GL_HEAD_DECL_LEN && getc (file) == _C4D2GL_HEAD_DECL[pos])
+	while (pos < _C4D2C_HEAD_DECL_LEN && getc (file) == _C4D2C_HEAD_DECL[pos])
 	{
 		pos++;
 	}
-	return pos != _C4D2GL_HEAD_DECL_LEN;
+	return pos != _C4D2C_HEAD_DECL_LEN;
 }
 
-int _c4d2gl_ushort (FILE * file, unsigned short * out)
+int _c4d2c_ushort (FILE * file, unsigned short * out)
 {
 	return fscanf (file, "%hu", out) != 1;
 }
 
-int _c4d2gl_float (FILE * file, float * out)
+int _c4d2c_float (FILE * file, float * out)
 {
 	return fscanf (file, "%f", out) != 1;
 }
 
-const char * c4d2gl_errstr (int error)
+const char * c4d2c_errstr (int error)
 {
 	switch (error)
 	{
-		case C4D2GL_SUCCESS:
+		case C4D2C_SUCCESS:
 			return "Success";
-		case C4D2GL_IO_ERROR:
+		case C4D2C_IO_ERROR:
 			return "File IO error";
-		case C4D2GL_BAD_HEAD_DECL:
+		case C4D2C_BAD_HEAD_DECL:
 			return "Bad header declaration";
-		case C4D2GL_BAD_VERT_COUNT:
+		case C4D2C_BAD_VERT_COUNT:
 			return "Bad vertices count";
-		case C4D2GL_BAD_IND_COUNT:
+		case C4D2C_BAD_IND_COUNT:
 			return "Bad indices count";
-		case C4D2GL_BAD_VERT_DATA:
+		case C4D2C_BAD_VERT_DATA:
 			return "Bad vertices data";
-		case C4D2GL_BAD_IND_DATA:
+		case C4D2C_BAD_IND_DATA:
 			return "Bad indices data";
-		case C4D2GL_UNEXPECTED_TAIL:
+		case C4D2C_UNEXPECTED_TAIL:
 			return "Unexpected data at the end of the file";
 		default:
 			return "Unknown";
 	}
 }
 
-int c4d2gl_isformaterror (int error)
+int c4d2c_isformaterror (int error)
 {
-	return error & C4D2GL_FORMAT_ERROR_BIT;
+	return error & C4D2C_FORMAT_ERROR_BIT;
 }
 
-int c4d2gl (const char * filename, unsigned short * vert_count, float ** verts, unsigned short * ind_count, unsigned short ** inds)
+int c4d2c (const char * filename, unsigned short * vert_count, float ** verts, unsigned short * ind_count, unsigned short ** inds)
 {
 	FILE * file = fopen (filename, "r");
 	if (file)
@@ -85,21 +85,21 @@ int c4d2gl (const char * filename, unsigned short * vert_count, float ** verts, 
 		unsigned short *is;
 
 		/* Header declaration */
-		if (_c4d2gl_head_decl (file) || _c4d2gl_sep (file, _C4D2GL_FIELD_SEP))
+		if (_c4d2c_head_decl (file) || _c4d2c_sep (file, _C4D2C_FIELD_SEP))
 		{
-			return C4D2GL_BAD_HEAD_DECL;
+			return C4D2C_BAD_HEAD_DECL;
 		}
 
 		/* Vertices count */
-		if (_c4d2gl_ushort (file, &vc) || _c4d2gl_sep (file, _C4D2GL_FIELD_SEP))
+		if (_c4d2c_ushort (file, &vc) || _c4d2c_sep (file, _C4D2C_FIELD_SEP))
 		{
-			return C4D2GL_BAD_VERT_COUNT;
+			return C4D2C_BAD_VERT_COUNT;
 		}
 
 		/* Indices count */
-		if (_c4d2gl_ushort (file, &ic) || _c4d2gl_sep (file, _C4D2GL_FIELD_SEP))
+		if (_c4d2c_ushort (file, &ic) || _c4d2c_sep (file, _C4D2C_FIELD_SEP))
 		{
-			return C4D2GL_BAD_IND_COUNT;
+			return C4D2C_BAD_IND_COUNT;
 		}
 
 		/* Vertices */
@@ -109,21 +109,21 @@ int c4d2gl (const char * filename, unsigned short * vert_count, float ** verts, 
 			vs = malloc (sizeof (float) * vc);
 			if (!vs)
 			{
-				return C4D2GL_MALLOC_FAIL;
+				return C4D2C_MALLOC_FAIL;
 			}
 			for (int v = 0; v < vc; v++)
 			{
-				if (_c4d2gl_float (file, &vs[v]) || _c4d2gl_sep (file, _C4D2GL_VALUE_SEP))
+				if (_c4d2c_float (file, &vs[v]) || _c4d2c_sep (file, _C4D2C_VALUE_SEP))
 				{
 					free (vs);
-					return C4D2GL_BAD_VERT_DATA;
+					return C4D2C_BAD_VERT_DATA;
 				}
 			}
 		}
-		if (_c4d2gl_sep (file, _C4D2GL_FIELD_SEP))
+		if (_c4d2c_sep (file, _C4D2C_FIELD_SEP))
 		{
 			free (vs);
-			return C4D2GL_BAD_VERT_DATA;
+			return C4D2C_BAD_VERT_DATA;
 		}
 
 		/* Indices */
@@ -133,23 +133,23 @@ int c4d2gl (const char * filename, unsigned short * vert_count, float ** verts, 
 			is = malloc (sizeof (float) * ic);
 			if (!is)
 			{
-				return C4D2GL_MALLOC_FAIL;
+				return C4D2C_MALLOC_FAIL;
 			}
 			for (int i = 0; i < ic; i++)
 			{
-				if (_c4d2gl_ushort (file, &is[i]) || _c4d2gl_sep (file, _C4D2GL_VALUE_SEP))
+				if (_c4d2c_ushort (file, &is[i]) || _c4d2c_sep (file, _C4D2C_VALUE_SEP))
 				{
 					free (vs);
 					free (is);
-					return C4D2GL_BAD_IND_DATA;
+					return C4D2C_BAD_IND_DATA;
 				}
 			}
 		}
-		if (_c4d2gl_sep (file, _C4D2GL_FIELD_SEP))
+		if (_c4d2c_sep (file, _C4D2C_FIELD_SEP))
 		{
 			free (vs);
 			free (is);
-			return C4D2GL_BAD_IND_DATA;
+			return C4D2C_BAD_IND_DATA;
 		}
 
 		/* End of file */
@@ -157,7 +157,7 @@ int c4d2gl (const char * filename, unsigned short * vert_count, float ** verts, 
 		{
 			free (vs);
 			free (is);
-			return C4D2GL_UNEXPECTED_TAIL;
+			return C4D2C_UNEXPECTED_TAIL;
 		}
 
 		fclose (file);
@@ -167,10 +167,10 @@ int c4d2gl (const char * filename, unsigned short * vert_count, float ** verts, 
 		*ind_count = ic;
 		*inds = is;
 
-		return C4D2GL_SUCCESS;
+		return C4D2C_SUCCESS;
 	}
 	else
 	{
-		return C4D2GL_IO_ERROR;
+		return C4D2C_IO_ERROR;
 	}
 }
--- C4D2C.h
+++ C4D2C.h
@@ -1,22 +1,22 @@
-#ifndef _INCLUDE_C4D2GL_H
-#define _INCLUDE_C4D2GL_H
+#ifndef _INCLUDE_C4D2C_H
+#define _INCLUDE_C4D2C_H
 
-#define C4D2GL_SUCCESS 0
-#define C4D2GL_FORMAT_ERROR_BIT (1 << 4)
-#define C4D2GL_RUNTIME_ERROR_BIT (1 << 5)
-#define C4D2GL_IO_ERROR (1 | C4D2GL_RUNTIME_ERROR_BIT)
-#define C4D2GL_MALLOC_FAIL (2 | C4D2GL_RUNTIME_ERROR_BIT)
-#define C4D2GL_BAD_HEAD_DECL (1 | C4D2GL_FORMAT_ERROR_BIT)
-#define C4D2GL_BAD_VERT_COUNT (2 | C4D2GL_FORMAT_ERROR_BIT)
-#define C4D2GL_BAD_IND_COUNT (3 | C4D2GL_FORMAT_ERROR_BIT)
-#define C4D2GL_BAD_VERT_DATA (4 | C4D2GL_FORMAT_ERROR_BIT)
-#define C4D2GL_BAD_IND_DATA (5 | C4D2GL_FORMAT_ERROR_BIT)
-#define C4D2GL_UNEXPECTED_TAIL (6 | C4D2GL_FORMAT_ERROR_BIT)
+#define C4D2C_SUCCESS 0
+#define C4D2C_FORMAT_ERROR_BIT (1 << 4)
+#define C4D2C_RUNTIME_ERROR_BIT (1 << 5)
+#define C4D2C_IO_ERROR (1 | C4D2C_RUNTIME_ERROR_BIT)
+#define C4D2C_MALLOC_FAIL (2 | C4D2C_RUNTIME_ERROR_BIT)
+#define C4D2C_BAD_HEAD_DECL (1 | C4D2C_FORMAT_ERROR_BIT)
+#define C4D2C_BAD_VERT_COUNT (2 | C4D2C_FORMAT_ERROR_BIT)
+#define C4D2C_BAD_IND_COUNT (3 | C4D2C_FORMAT_ERROR_BIT)
+#define C4D2C_BAD_VERT_DATA (4 | C4D2C_FORMAT_ERROR_BIT)
+#define C4D2C_BAD_IND_DATA (5 | C4D2C_FORMAT_ERROR_BIT)
+#define C4D2C_UNEXPECTED_TAIL (6 | C4D2C_FORMAT_ERROR_BIT)
 
-const char * c4d2gl_errstr (int error);
+const char * c4d2c_errstr (int error);
 
-int c4d2gl_isformaterror (int error);
+int c4d2c_isformaterror (int error);
 
-int c4d2gl (const char *filename, unsigned short * vert_count, float ** verts, unsigned short * ind_count, unsigned short ** inds);
+int c4d2c (const char *filename, unsigned short * vert_count, float ** verts, unsigned short * ind_count, unsigned short ** inds);
 
 #endif
