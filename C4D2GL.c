48df7a68fb5b82b1ea01ea0b5c75f24bbb06e991
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2018-04-01 12:13:41 +0200

    Before stashing

--- C4D2GL.c
+++ C4D2GL.c
@@ -1,20 +1,29 @@
 #include "C4D2GL.h"
 #include <stdio.h>
+#include <limits.h>
+#include <ctype.h>
 
-
-#define _C4D2GL_HEADER "C4D2GL v1.0"
+#define _C4D2GL_HEAD_DECL "C4D2GL v1.0"
+#define _C4D2GL_HEAD_DECL_LEN (sizeof(_C4D2GL_HEAD_DECL) / sizeof(char) - 1)
 
 #define _C4D2GL_EOS -7
+#define _C4D2GL_PARSE_SUCCESS 0
+#define _C4D2GL_PARSE_FAILURE 1
+#define _C4D2GL_MAX_INT USHRT_MAX
 
-typedef int (*_c4d2gl_stream_pop_func)(void * stream);
+typedef int (*_c4d2gl_pop_func)(void * stream);
 
-int _c4d2gl_file_pop (FILE * file);
+int _c4d2gl_pop_file (FILE * file);
 
-int _c4d2gl_charp_pop (char ** charp);
+int _c4d2gl_pop_charp (char ** charp);
 
-int _c4d2gl_head(_c4d2gl_stream_pop_func stream_pop_func, void * stream, )
+int _c4d2gl_parse_head_decl (_c4d2gl_pop_func pop_stream_func, void * stream, char separator);
 
-int _c4d2gl_file_pop (FILE * file)
+int _c4d2gl_parse_unsigned_integer (_c4d2gl_pop_func pop_stream_func, void * stream, char separator, int max, int * out);
+
+int _c4d2gl_head (_c4d2gl_pop_func pop_stream_func, void * stream, int * vertices_count, int * indices_count);
+
+int _c4d2gl_pop_file (FILE * file)
 {
 	int ch = getc (file);
 	if (ch == EOF)
@@ -34,7 +43,7 @@ int _c4d2gl_file_pop (FILE * file)
 	}
 }
 
-int _c4d2gl_charp_pop (char ** charp)
+int _c4d2gl_pop_charp (char ** charp)
 {
 	char ch = *charp[0];
 	if (ch == '\0')
@@ -45,5 +54,58 @@ int _c4d2gl_charp_pop (char ** charp)
 	{
 		(*charp)++;
 		return ch;
+	}
+}
+
+int _c4d2gl_parse_head_decl (_c4d2gl_pop_func pop, void * stream, char sep)
+{
+	int pos, ch;
+	pos = 0;
+	ch = pop (stream);
+	while (pos < _C4D2GL_HEAD_DECL_LEN && ch == _C4D2GL_HEAD_DECL[pos])
+	{
+		pos++;
+	}
+	if (pos == _C4D2GL_HEAD_DECL_LEN && pop(stream) == sep)
+	{
+		return _C4D2GL_PARSE_SUCCESS;
+	}
+	else
+	{
+		return _C4D2GL_PARSE_FAILURE;
+	}
+}
+
+int _c4d2gl_parse_unsigned_integer (_c4d2gl_pop_func pop, void * stream, char sep, int max, int * out)
+{
+	unsigned int val;
+	int ch;
+	val = 0;
+	ch = pop (stream);
+	if (isdigit (ch))
+	{
+		while (isdigit (ch))
+		{
+			unsigned int oldval = val;
+			val = val * 10 + ch - '0';
+			if (val < oldval || val > max)
+			{
+				return _C4D2GL_PARSE_FAILURE;
+			}
+			ch = pop (stream);
+		}
+		if (ch == sep)
+		{
+			*out = val;
+			return _C4D2GL_PARSE_SUCCESS;
+		}
+		else
+		{
+			return _C4D2GL_PARSE_FAILURE;
+		}
+	}
+	else
+	{
+		return _C4D2GL_PARSE_FAILURE;
 	}
 }
--- C4D2GL.h
+++ C4D2GL.h
@@ -2,12 +2,11 @@
 #define _INCLUDE_C4D2GL_H
 
 #define C4D2GL_SUCCESS 0
-#define C4D2GL_BAD_HEADER -1
-#define C4D2GL_UNEXPECTED_END -2
-#define C4D2GL_UNEXPECTED_TOKEN -3
-#define C4D2GL_VALUE_OVERFLOW -4
-#define C4D2GL_IO_ERROR -5
-#define C4D2GL_MALLOC_FAIL -6
+#define C4D2GL_BAD_HEADER_DECL -1
+#define C4D2GL_IO_ERROR -2
+#define C4D2GL_MALLOC_FAIL -3
+
+const char * c4d2gl_result_string (int result);
 
 int c4d2gl_count(const char * data, int * vertices_count, int * indices_count);
 
