a18299595f71332e4ae36410b899a0a51496ec61
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2018-04-01 11:21:51 +0200

    Add gitignore

--- C4D2GL.c
+++ C4D2GL.c
@@ -1,32 +1,49 @@
 #include "C4D2GL.h"
+#include <stdio.h>
 
-#define HEADER "C4D2GL v1.0"
 
-int c4d2gl_head(const char * data, int * vertCount, int * intCount, int * length);
+#define _C4D2GL_HEADER "C4D2GL v1.0"
 
-int c4d2gl_head(const char * data, int * vertCount, int * intCount, int * length)
+#define _C4D2GL_EOS -7
+
+typedef int (*_c4d2gl_stream_pop_func)(void * stream);
+
+int _c4d2gl_file_pop (FILE * file);
+
+int _c4d2gl_charp_pop (char ** charp);
+
+int _c4d2gl_head(_c4d2gl_stream_pop_func stream_pop_func, void * stream, )
+
+int _c4d2gl_file_pop (FILE * file)
 {
-    const int headerLen = sizeof(HEADER) / sizeof(char) - 1;
-    int pos = 0;
-    char curr = data[0];
-    while (curr != '\0' && pos < headerLen && curr == HEADER[pos])
-    {
-        curr = data[pos++];
-    }
-    if (pos != headerLen || data[pos] != '\n')
-    {
-        return -1;
-    }
-    
+	int ch = getc (file);
+	if (ch == EOF)
+	{
+		if (ferror (file))
+		{
+			return C4D2GL_IO_ERROR;
+		}
+		else
+		{
+			return _C4D2GL_EOS;
+		}
+	}
+	else
+	{
+		return ch;
+	}
 }
 
-int c4d2gl_count(const char * data, int * vertCount, int * intCount)
+int _c4d2gl_charp_pop (char ** charp)
 {
-
+	char ch = *charp[0];
+	if (ch == '\0')
+	{
+		return _C4D2GL_EOS;
+	}
+	else
+	{
+		(*charp)++;
+		return ch;
+	}
 }
-
-int c4d2gl_fill(const char * data, float * verts, unsigned short * inds);
-
-int c4d2gl_malloc(const char * data, float ** verts, unsigned short ** inds);
-
-int c4d2gl_file_malloc(const char * filename, float ** verts, unsigned short ** inds);
