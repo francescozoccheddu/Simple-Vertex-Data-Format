85614e2268b4abbc99e2e6a351203b3640622810
Francesco Zoccheddu <zoccheddu.fra@gmail.com>
2018-04-01 16:06:03 +0200

    Update export script

--- C4D2GL_Export.py
+++ C4D2GL_Export.py
@@ -4,9 +4,14 @@ from c4d import documents
 import traceback
 import os
 
+header = 'C4D2GL v1.0'
+fieldsep = '\n'
+valuesep = ','
+scale = 1.0 / 100.0
+
 class Vertex:
     
-    def __init__(self, pos = c4d.Vector(), norm = c4d.Vector()):
+    def __init__(self, pos, norm):
         self.pos = pos
         self.norm = norm
         
@@ -22,26 +27,10 @@ class Mesh:
     def Add(self, vert):
         for i, exvert in enumerate(self.verts):
             if exvert == vert:
-                # Duplicated vertex
                 self.inds.append(i)
                 return
-        # New vertex
         self.inds.append(len(self.verts))
         self.verts.append(vert)
-        
-    def GetIndArray(self):
-        return self.inds
-        
-    def MakePosNormArray(self, scale = 1.0):
-        va = []
-        for vert in self.verts:
-            va.append(vert.pos.x * scale)
-            va.append(vert.pos.y * scale)
-            va.append(vert.pos.z * scale)
-            va.append(vert.norm.x)
-            va.append(vert.norm.y)
-            va.append(vert.norm.z)
-        return va  
       
                 
 def MakeMesh(obj):
@@ -49,34 +38,34 @@ def MakeMesh(obj):
         raise ValueError('Polygon is not triangulated')
     
     mesh = Mesh()
-    
     polys = obj.GetAllPolygons()
     points = obj.GetAllPoints()
     norms = GetPolyObjNormals(obj)
     
     n = 0
     for poly in polys:
-        mesh.Add(Vertex(points[poly.a], norms[n]))
+        mesh.Add(Vertex(points[poly.a] * scale, norms[n]))
         n += 1
-        mesh.Add(Vertex(points[poly.b], norms[n]))
+        mesh.Add(Vertex(points[poly.b] * scale, norms[n]))
         n += 1
-        mesh.Add(Vertex(points[poly.c], norms[n]))
+        mesh.Add(Vertex(points[poly.c] * scale, norms[n]))
         n += 1
         
     return mesh
 
+def CalcTriangleNormal(a, b, c):
+    u = b - a
+    v = c - a
+    norm = u.Cross(v)
+    return norm.GetNormalized()
+
 def GetPolyObjNormals(obj):
     norms = obj.CreatePhongNormals()
     if norms is None:
-        # Object has no Phong tag
         norms = []
         polys = obj.GetAllPolygons()
         for poly in polys:
-            # Calculate normal for triangle
-            u = poly.b - poly.a
-            v = poly.c - poly.a
-            norm = u.Cross(v)
-            norms.add(norm)
+            norms.add(CalcTriangleNormal(poly.a, poly.b, poly.c))
     return norms
     
 def IsPolyObj(obj):
@@ -88,48 +77,56 @@ def IsPolyObjTriangulated(obj):
             if not poly.IsTriangle():
                 return False
         return True    
+
+def SerializeVec(v):
+    return str(v.x) + valuesep + str(v.y) + valuesep + str(v.z) + valuesep
+
+def SerializePolyObj(obj):
+    if not IsPolyObj(obj):
+        raise ValueError('Object is not a PolygonObject')
+    mesh = MakeMesh(obj)
+    ser = header
+    ser += fieldsep
+    ser += str(len(mesh.verts * 6))
+    ser += fieldsep
+    ser += str(len(mesh.inds))
+    ser += fieldsep
+    for v in mesh.verts:
+        ser += SerializeVec(v.pos) + SerializeVec(v.norm)
+    ser += fieldsep
+    for i in mesh.inds:
+        ser += str(i) + valuesep
+    ser += fieldsep
+    return ser
     
-def ToSVString(array, separator):
-    svs = ''
-    for el in array:
-        svs += str(el) + separator
-    return svs.rstrip(separator)
-
-def ToCArray(array, varname, vartype):
-    vararr = 'const ' + vartype + ' ' + varname + '[] = {' + ToSVString(array,',') + '};'
-    varnum = 'const int ' + varname + '_size = ' + len(array) + ';'
-    return vararr + '\n' + varnum + '\n'
-
-def ToCppArray(array, varname, vartype):
-    return 'constexpr std::vector<' + vartype + '> ' + varname + '{' + ToSVString(array,',') + '};\n'
-
 def main():
-    unit = 100.0
-    scale = 1.0 / unit
-    base = 'C:/Users/zocch/Desktop/'
-    
+    print('C4D2GL v1.0 exporter script by Francesco Zoccheddu')
+
     doc = documents.GetActiveDocument()
+    path = doc.GetDocumentPath()
     objs = doc.GetActiveObjects(c4d.GETACTIVEOBJECTFLAGS_CHILDREN)
     
+    print('Exporting to path "' + path + '"')
+    allok = True
     for obj in objs:
-        if IsPolyObj(obj):
-            name = obj.GetName()
-            mesh = MakeMesh(obj)
-            va = mesh.MakePosNormArray(scale)
-            ia = mesh.GetIndArray()
-            txt = '#include <vector>\n'
-            txt += ToCppArray(va,name + '_verts','float')
-            txt += ToCppArray(ia,name + '_inds','short')
-            fname = os.path.join(base,name)
-            f = open(fname,'w')
-            f.write(txt)
-            f.close()
+        try:
+            ser = SerializePolyObj(obj)
+            filename = os.path.join(path, obj.GetName())
+            outfile = open(filename, 'w')
+            outfile.write(ser)
+            outfile.close()
+            print('Object "' + obj.GetName() + '" exported successfully')
+        except ValueError as e:
+            allok = False
+            print('Exception while exporting object "' + obj.GetName() + '": ' + str(e))
+    
+    print('Exporting process finished successfully')
+    
+    if allok:
+        gui.MessageDialog('No error occurred')
+    else:
+        gui.MessageDialog('Some errors occured')
+
 
 if __name__=='__main__':
-    try:
-        main()
-        gui.MessageDialog('Done')
-    except Exception as e:
-        traceback.print_exc()
-        gui.MessageDialog(str(e))
-        
+    main()        
