8e2d86ec5de7ff233ccffa9b4d8047991e480080
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2018-04-01 14:32:44 +0200

    Header test

--- C4D2GL.h
+++ C4D2GL.h
@@ -2,18 +2,22 @@
 #define _INCLUDE_C4D2GL_H
 
 #define C4D2GL_SUCCESS 0
-#define C4D2GL_BAD_HEADER_DECL -1
-#define C4D2GL_IO_ERROR -2
-#define C4D2GL_MALLOC_FAIL -3
+#define C4D2GL_FORMAT_ERROR_BIT (1 << 4)
+#define C4D2GL_RUNTIME_ERROR_BIT (1 << 5)
+#define C4D2GL_IO_ERROR (1 | C4D2GL_RUNTIME_ERROR_BIT)
+#define C4D2GL_MALLOC_FAIL (2 | C4D2GL_RUNTIME_ERROR_BIT)
+#define C4D2GL_BAD_HEAD_DECL (1 | C4D2GL_FORMAT_ERROR_BIT)
+#define C4D2GL_BAD_VERT_COUNT (2 | C4D2GL_FORMAT_ERROR_BIT)
+#define C4D2GL_BAD_IND_COUNT (3 | C4D2GL_FORMAT_ERROR_BIT)
+#define C4D2GL_BAD_VERT (4 | C4D2GL_FORMAT_ERROR_BIT)
+#define C4D2GL_EXPECTED_VERT (5 | C4D2GL_FORMAT_ERROR_BIT)
+#define C4D2GL_BAD_IND (6 | C4D2GL_FORMAT_ERROR_BIT)
+#define C4D2GL_EXPECTED_IND (7 | C4D2GL_FORMAT_ERROR_BIT)
 
-const char * c4d2gl_result_string (int result);
+const char * c4d2gl_errstr (int error);
 
-int c4d2gl_count(const char * data, int * vertices_count, int * indices_count);
+int c4d2gl_isformaterror (int error);
 
-int c4d2gl_fill(const char * data, float * vertices, unsigned short * indices);
-
-int c4d2gl_malloc(const char * data, float ** vertices, unsigned short ** indices);
-
-int c4d2gl_file_malloc(const char * filename, float ** vertices, unsigned short ** indices);
+int c4d2gl (const char *filename, unsigned short * vert_count, float ** verts, unsigned short * ind_count, unsigned short ** inds);
 
 #endif
