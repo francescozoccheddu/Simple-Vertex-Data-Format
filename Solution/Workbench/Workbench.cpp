6ffe5062f42d4f0c394558af46bced56e211cecf
Francesco Zoccheddu <zoccheddu.fra@gmail.com>
2018-04-27 21:13:59 +0200

    Parser working

--- Solution/Workbench/Workbench.cpp
+++ Solution/Workbench/Workbench.cpp
@@ -13,7 +13,7 @@ int main ()
 		SVDF::Parser p (file);
 		while (!p.is_eof ())
 		{
-			//std::cout << p.next_list () << std::endl;
+			p.next_declaration ();
 			while (p.is_in_data_section ())
 			{
 				std::cout << p.next_value<float> () << ",";
